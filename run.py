317d9f795c6dec920dd0a04c60856d67a74a70f3
alychinque <alychinque@gmail.com>
2020-12-29 03:52:16 +0000

    Update run.py.

--- run.py
+++ run.py
@@ -10,12 +10,12 @@ def index():
     return render_template("index.html")
 
 
-@app.route("about")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
 
-@app.route("contact")
+@app.route("/contact")
 def contact():
     return render_template("contact.html")
 
